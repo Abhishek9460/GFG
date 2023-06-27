c04990e14ca8a0065d4516cdeab1cd64c1fac7b0
Abhishek Kumar <98384405+Abhishek9460@users.noreply.github.com>
2023-06-27 23:26:04 +0530

    Added solution - LeetHub

--- Trapping Rain Water - GFG/trapping-rain-water.py
+++ Trapping Rain Water - GFG/trapping-rain-water.py
@@ -7,17 +7,14 @@ class Solution:
         right_max = [0] * n
         water = 0
 
-    # Calculate the maximum height to the left of each block
         left_max[0] = arr[0]
         for i in range(1, n):
             left_max[i] = max(left_max[i - 1], arr[i])
 
-    # Calculate the maximum height to the right of each block
         right_max[n - 1] = arr[n - 1]
         for i in range(n - 2, -1, -1):
             right_max[i] = max(right_max[i + 1], arr[i])
 
-    # Calculate the trapped water at each block
         for i in range(n):
             water += min(left_max[i], right_max[i]) - arr[i]
 
